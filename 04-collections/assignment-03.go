e4fd277a265ec2f601586f24f69bdd58218ca934
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-20 00:40:07 +0530

    assignments 03 & 04 - solutions

--- 04-collections/assignment-03.go
+++ 04-collections/assignment-03.go
@@ -7,6 +7,21 @@ func main() {
 	fmt.Println(primeNos)
 }
 
-func generatePrimes( /*  */ ) /*  */ {
+func generatePrimes(start, end int) []int {
+	primeNos := []int{}
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primeNos = append(primeNos, no)
+		}
+	}
+	return primeNos
+}
 
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
