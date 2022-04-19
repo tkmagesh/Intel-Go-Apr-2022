d84b49cd814533ad2e3159ecacc39d25c00443be
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-19 23:22:48 +0530

    slicing

--- 04-collections/slice.go
+++ 04-collections/slice.go
@@ -6,6 +6,17 @@ func main() {
 	var nos []int = []int{3, 1, 4, 2, 5}
 	fmt.Printf("nos = %v, len(nos) = %d, cap(nos) = %d\n", nos, len(nos), cap(nos))
 
+	/*
+		slicing
+		nos[lo:hi] => from lo to hi-1
+		nos[lo:] => from lo to last value
+		nos[:hi] => from 0 to hi - 1
+	*/
+
+	fmt.Println("nos[2:4] => ", nos[2:4])
+	fmt.Println("nos[2:] => ", nos[2:])
+	fmt.Println("nos[:4] => ", nos[:4])
+
 	var count int
 	fmt.Println("Enter the number of values")
 	fmt.Scanln(&count)
