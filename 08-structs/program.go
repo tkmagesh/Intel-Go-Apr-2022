cb98873845b3227b5e60201cff6f2c13d8a6f40a
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-20 03:37:30 +0530

    structs intro

---
+++ 08-structs/program.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	Id     int
+	Name   string
+	Salary float32
+}
+
+func main() {
+	//var emp Employee
+	//var emp Employee = Employee{100, "Magesh", 10000}
+	//var emp Employee = Employee{Id: 100, Name: "Magesh"}
+	/*
+		var emp Employee = Employee{
+			Id:   100,
+			Name: "Magesh",
+		}
+	*/
+	emp := Employee{
+		Id:     100,
+		Name:   "Magesh",
+		Salary: 10000,
+	}
+	fmt.Printf("%#v\n", emp)
+	fmt.Println(Format(emp))
+	fmt.Println("After 10% bonus")
+	ApplyBonus(&emp, 10)
+	fmt.Println(Format(emp))
+}
+
+func Format(emp Employee) string {
+	return fmt.Sprintf("Id = %d, Name = %q, Salary = %f", emp.Id, emp.Name, emp.Salary)
+}
+
+func ApplyBonus(emp *Employee, bonusPercentage float32) {
+
+	//(*emp).Salary = (*emp).Salary + (*emp).Salary*(bonusPercentage/100)
+	emp.Salary = emp.Salary + emp.Salary*(bonusPercentage/100)
+}
