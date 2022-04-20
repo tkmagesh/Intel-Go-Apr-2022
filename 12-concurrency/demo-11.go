e3fbefe348e2193aa5aa0bcbff29598d68caed3b
Magesh Kuppan <tkmagesh77@gmail.com>
2022-04-21 03:03:37 +0530

    more on channels

--- 12-concurrency/demo-11.go
+++ 12-concurrency/demo-11.go
@@ -2,25 +2,21 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 //Share memory by communicting
 
 func main() {
-	var wg sync.WaitGroup
+
 	ch := make(chan int)
 	fmt.Println("main started")
-	wg.Add(1)
-	go add(100, 200, &wg, ch)
+	go add(100, 200, ch)
 	result := <-ch
-	wg.Wait()
 	fmt.Println("Result =", result)
 	fmt.Println("main completed")
 }
 
-func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+func add(x, y int, ch chan int) {
 	result := x + y
 	ch <- result
-	wg.Done()
 }
